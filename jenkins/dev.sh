dc589f2e377bf9b32568582e35c95fcf84d08401
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2018-06-08 10:19:24 +0300

    fix

--- jenkins/dev.sh
+++ jenkins/dev.sh
@@ -11,4 +11,4 @@ tar -cf ${__tar_name} docker-build-cache/ lib/ pom.xml Dockerfile daytrader-ee7-
 gzip ${__tar_name}
 
 #move source to HTTP file server path
-mv ${__tar_name}* ${FILE_SERVER_PATH}/'''
+mv ${__tar_name}* ${FILE_SERVER_PATH}/
