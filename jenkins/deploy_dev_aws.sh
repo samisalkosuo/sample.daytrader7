0585726214b7a59c8d82b1b1bb8d50440214668c
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2018-06-11 13:08:22 +0300

    updated

--- jenkins/deploy_dev_aws.sh
+++ jenkins/deploy_dev_aws.sh
@@ -7,7 +7,7 @@ __tar_name=${APP_NAME}-${__ver}.tar
 echo "Version: ${__ver}"
 echo "tar and gzip source"
 
-tar -cf ${__tar_name} docker-build-cache/ lib/ pom.xml Dockerfile daytrader-ee7-ejb/ daytrader-ee7-web/ daytrader-ee7-wlpcfg/ daytrader-ee7/ 
+tar -cf ${__tar_name} docker-build-cache/ scripts/ lib/ pom.xml Dockerfile daytrader-ee7-ejb/ daytrader-ee7-web/ daytrader-ee7-wlpcfg/ daytrader-ee7/ 
 gzip ${__tar_name}
 
 __gz_name=${__tar_name}.gz
@@ -51,7 +51,7 @@ echo "Service ID ${__service_id} Instance ID ${__instance_id}"
 #sleep a moment before continuing
 echo "Getting status..."
 sleep 3
-python ${__work_dir}/get_service_status.py ${__service_id} ${__instance_id} &> /dev/null
+python ${__work_dir}/get_service_status.py ${__service_id} ${__instance_id} 2> /dev/null
 __status=$(cat DEPLOYMENT_STATUS)
 
 echo "Deployment status: " ${__status}
