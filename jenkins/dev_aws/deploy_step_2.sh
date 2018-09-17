a68d38631859517ae9dc0634a9ab0fb6319d79b8
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2018-09-17 13:57:27 +0300

    updated

--- jenkins/dev_aws/deploy_step_2.sh
+++ jenkins/dev_aws/deploy_step_2.sh
@@ -4,11 +4,10 @@ set -o errexit
 
 source jenkins/dev_aws/variables.sh
 
-
 # read varible from file
 export DOCKER_IMAGE_TAR_FILE=$(cat DOCKER_IMAGE_TAR_FILE)
 
 #upload file to object storage
+#and store download url to file DOWNLOAD_URL.txt
 sh ${__work_dir}/upload_to_object_storage.sh
 
-#and store download url to file DOWNLOAD_URL.txt
