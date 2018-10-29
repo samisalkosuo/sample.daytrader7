8844a84dd6818bd640fa57cbef3e9407d09e323d
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2018-10-29 14:57:32 +0200

    updated

--- jenkins/dev_aws/get_service_status.py
+++ jenkins/dev_aws/get_service_status.py
@@ -24,19 +24,11 @@ CAM_URL=os.environ['CAM_URL']
 
 def getIpAddress(instance):
     #get template output IP address
-    #instanceJson=instance["template_mapping"]
-    #print("template_mapping instanceJson:")
-    #print(instanceJson)
-    #templateMappingId=json.dumps(instanceJson)
-    #templateMappingId=templateMappingId.replace("{","").replace('"',"")
-    #templateMappingId=templateMappingId[0:templateMappingId.find(":")]
-    #print("templateMappingId:")
-    #print(templateMappingId)
-    #hardcoded in service file
+    #id is hardcoded in service file
     templateMappingId="daytrade4913dc32"
     instanceJson=instance["activity_deployment_details"]["instance"][templateMappingId]
     ipAddress=instanceJson["template_outputs_IP address"]
-    print("template_mapping IP address:")
+    print("template_outputs_IP address:")
     print(ipAddress)
     return ipAddress
 
